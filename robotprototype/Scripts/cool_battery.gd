fe27bf2e86dc5488e3ea0a2aa621a28986822a32
hbohrjah <hbohrjah@ucsc.edu>
2025-01-27 20:29:59 -0800

    combat changes
    Integrated both UI and player action animations.

--- robotprototype/Scripts/cool_battery.gd
+++ robotprototype/Scripts/cool_battery.gd
@@ -10,6 +10,16 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 		
 		pass # Replace with function body.
 	
+	if area.is_in_group("meleeRange"):
+		var knock_dir = area.global_position.direction_to(global_position)
+		print("Collision normal:", knock_dir)
+		#var impulse = knock_dir * strength
+		#print("Collided with player fist")
+		var knockX = knock_dir.x *400
+		apply_central_impulse(Vector2(knockX,0))
+		pass # Replace with function body.
+
+	
 	if area.is_in_group("targetArc"):
 		print("Collided with a circle")
 		pass # Replace with function body.
--- robotprototype/Scripts/player.gd
+++ robotprototype/Scripts/player.gd
@@ -1,63 +1,88 @@
 extends CharacterBody2D
 
 @onready var space_state = get_world_2d().direct_space_state
+#@onready var mesh_instance_2d: Sprite2D = $Area2D/MeshInstance2D
+@onready var animations: AnimationPlayer = $Area2D/MeshInstance2D/AnimationPlayer
+@onready var main = $".."
+
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 const pos = Vector2(0,0)
+const max_health : float = 100.0
+const max_energy : float = 100.0
 
 var max_distance : float = 100.0
-var max_health : float = 100.0
-var max_energy : float = 100.0
+var curr_health : float = 100.0
+var curr_energy : float = 100.0
 var energy_reduction_rate : float = 0.01
 var FOV_scalar = 2*PI/90
 var time_elapsed = 0.0 
 var attackable = 0
 var invincible = false
+var rendered = false
+var has_helmet01 = false
+var pl_animations
+var dir_velocity
+
+func _ready():
+	pl_animations = $AnimationTree.get("parameters/playback")
 
 func _process(delta: float) -> void:
 	if self != null:
-		if max_health == 0:
+		if curr_health == 0:
 			self.queue_free()
 	time_elapsed += delta  # Increment the time elapsed by the frame time
 
 	if time_elapsed >= 1.0:  # If a second has passed
-		max_energy -= (max_energy*energy_reduction_rate)
+		curr_energy -= energy_reduction_rate
 		time_elapsed = 0.0  # Reset the time counter
-		print(max_energy)
-	
+	#print(curr_energy)
+	#print(has_helmet01)
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	
+	if(!(main.paused)):
+		# Add the gravity.
+		if not is_on_floor():
+			velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		# Handle jump.
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction := Input.get_axis("ui_left", "ui_right")
+		if direction:
+			#$AnimationTree.get("parameters/playback").travel("Walk")
+			dir_velocity = velocity
+			$AnimationTree.set("parameters/Idle/blend_position", velocity)
+			velocity.x = direction * SPEED
+		else:
+			pl_animations.travel("Idle")
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
-	targetAreaCast()
+		move_and_slide()
+		melee_attack()
+		targetAreaCast()
 
+func melee_attack():
+	if Input.is_action_pressed("attack") and !(has_helmet01):
+		pl_animations.travel("Punch")
+		$AnimationTree.set("parameters/Punch/blend_position", dir_velocity)
 
 func targetAreaCast():
-	if Input.is_action_pressed("slow-mo") and max_energy >= 1:
-		attackable = 0
-		set_target_area(get_FOV_circle(position, 60))
-		energy_reduction_rate = 0.2
-		Engine.time_scale = 0.5
-	else:
-		clear_target_area()
-		energy_reduction_rate = 0.1
-		Engine.time_scale = 1
+	if has_helmet01:
+		if Input.is_action_pressed("slow-mo") and curr_energy >= 1:
+			attackable = 0
+			set_target_area(get_FOV_circle(position, 60))
+			energy_reduction_rate = 5
+			Engine.time_scale = 0.5
+		else:
+			if rendered:
+				clear_target_area()
+			energy_reduction_rate = 0.1
 		
 func get_FOV_circle(from:Vector2, radius):
 	var angle = FOV_scalar
@@ -102,15 +127,36 @@ func get_FOV_circle(from:Vector2, radius):
 
 func set_target_area(points:PackedVector2Array):
 	$targetArea.polygon = points
+	rendered = true
 	
 func clear_target_area():
+	Engine.time_scale = 1
 	set_target_area(PackedVector2Array())
+	rendered = false
+
+func enemy_knockbackfx(enemy: Area2D, strength: float):
+	pl_animations.travel("flash_dmg")
+	
+	# Issue with how the normals are obtained
+	#var knock_dir = enemy.global_position.direction_to(global_position)
+	#print("Collision normal:", knock_dir)
+	#var impulse = knock_dir * strength
+	#velocity += impulse
+	
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
+	
+	var enemy = area.get_parent()
 	if area.is_in_group("enemy") and !invincible:
-		max_health -= 10
-		print("Collided with a enemy ", max_health)
+		curr_health -= 10
+		enemy_knockbackfx(area, 500)
+		print("Collided with a enemy ", curr_health)
 	elif area.is_in_group("enemy") and invincible:
-		print("I should not recieve dmg", max_health)
-		area.get_parent().queue_free()
+		#print("I should not recieve dmg", max_health)
+		var orbPos = area.global_position
+		enemy.queue_free()
+		var orbScene = load("res://orb.tscn")
+		var orbInst = orbScene.instantiate()
+		self.get_parent().call_deferred("add_child", orbInst)
+		orbInst.global_position = orbPos
 	pass # Replace with function body.

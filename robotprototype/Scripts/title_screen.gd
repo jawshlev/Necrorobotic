dd9c6c31c6d96393162eec3173b1e2ba6168163f
Jaren Kawai <jkawai@ucsc.edu>
2025-01-31 20:14:51 -0800

    UI health bar and energy bar added, folder for all assets, navigation to controls and credits screen from title screen

--- robotprototype/Scripts/title_screen.gd
+++ robotprototype/Scripts/title_screen.gd
@@ -2,10 +2,16 @@ extends Control
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
-	pass # Replace with function body.
 
 
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
-	pass # Replace with function body.
+
+
+func _on_controls_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/controls_screen.tscn")
+
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/credits_screen.tscn")

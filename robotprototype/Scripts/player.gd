8a1a5a444ca2a65387cadbeec266310b50b3a41b
NewsFlashGameCrash <167850701+NewsFlashGameCrash@users.noreply.github.com>
2025-02-02 15:29:29 -0800

    Fixing a few errors from the merge

--- robotprototype/Scripts/player.gd
+++ robotprototype/Scripts/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 var speed = 150.0 #Player's speed
 var jump_velocity = -250.0 #Player's Jump Height
-var robot_parts = [0, 0, 1, 0] #Which robo-parts the player has. Order is Legs, Arms, Chest, Head
+var robot_parts = [0, 0, 0, 0] #Which robo-parts the player has. Order is Legs, Arms, Chest, Head
 var facing_right = 1 #Which way the player is facing
 var can_dash = true #Tracks cooldown on dash
 var double_jump = false #Tracks double jumps
